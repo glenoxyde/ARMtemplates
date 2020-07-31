13499cd6a3aaf5f898875d9921ffcbfe084f3546
glenoxyde <61327386+glenoxyde@users.noreply.github.com>
2020-07-31 11:35:27 +0200

    Update sample.php

--- sample.php
+++ sample.php
@@ -8,4 +8,9 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "adming", "pwd" => "{your_password_here}", "Database" => "kyostestdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dbsever.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
